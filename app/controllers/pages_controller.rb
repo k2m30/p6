3f81c2102742874cee77296343e4c78ca8d25d0c
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2018-09-14 19:38:42 +0300

    flying

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,12 @@
 class PagesController < ApplicationController
   def main
-    # @image = Image.new('flying.svg')
-    @image = Image.new('risovaka007_003.svg')
+    @image = Image.new('flying.svg')
+    # @image = Image.new('risovaka007_003.svg')
     layer = params[:layer]
     if layer.nil?
       @svg = @image.name
     else
       @svg = @image.svg.build_svg(layer)
-
     end
   end
 end
