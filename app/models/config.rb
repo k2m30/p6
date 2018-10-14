9ab99492f13a2da8cad563f38f0acae885451a41
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2018-10-14 14:41:23 +0300

    config read and write

--- app/models/config.rb
+++ app/models/config.rb
@@ -1,14 +1,17 @@
 class Config
   class << self
-    file_name = Rails.root&.join('config','config.yml') || '/Users/user/projects/p6/config/config.yml'
+    file_name = Rails.root&.join('config', 'config.yml') || '/Users/user/projects/p6/config/config.yml'
     YAML.load(File.open(file_name)).keys.each do |method|
       define_method :"#{method}=" do |value|
         Redis.new.set method, value
+        hash = YAML.load_file(file_name)
+        hash[method]['value'] = value
+        File.write(file_name, hash.to_yaml)
       end
       define_method :"#{method}" do
         value = Redis.new.get(method)
         if value.nil?
-          value = YAML::load_file(file_name)[method]
+          value = YAML::load_file(file_name)[method]['value']
           Redis.new.set method, value
         end
         value.to_f
