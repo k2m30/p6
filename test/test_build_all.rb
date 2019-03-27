4749e1aea33ace0a01ae5babf7a74b49b8c2bd0a
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2019-03-27 21:52:41 +0300

    tests added and failed

---
+++ test/test_build_all.rb
@@ -0,0 +1,41 @@
+require "minitest/autorun"
+
+class TestMeme < Minitest::Test
+  def setup
+  end
+
+  def test_builds_risovaka
+    file_name = 'risovaka007_003.svg'
+    path = Rails.root.join('public')
+    @image = SVG.new(file_name, path)
+    @image.get_layer_names
+    @image.layers.keys.each do |name|
+      p 'Layer name: ' + name
+      p '---------------'
+
+      @image.get_layer(name)
+      Layer.build(name)
+
+      p name + ' tested'
+      p '---------------'
+    end
+  end
+
+  def test_builds_flying
+    file_name = 'flying.svg'
+    path = Rails.root.join('public')
+    @image = SVG.new(file_name, path)
+    @image.get_layer_names
+    @image.layers.keys.each do |name|
+      p 'Layer name: ' + name
+      p '---------------'
+
+      @image.get_layer(name)
+      Layer.build(name)
+
+      p name + ' tested'
+      p '---------------'
+    end
+  end
+
+end
